73a2f8654d46353b1b8a370ccdbef6230fc09706
Adrian_Peña <adrianchooo18@gmail.com>
2021-11-15 16:54:43 -0500

    first commit

---
+++ control.js
@@ -0,0 +1,11 @@
+//Variables and start
+const GAME = function() {
+  let time = 50;
+  let movement = 20;
+  let movementBar = 20;
+  let width = document.documentElement.clientWidth - movement;
+  let height = document.documentElement.clientHeight - movement;
+  let controlGame;
+  let player1;
+  let player2;
+}
